bdd605d19a33ae4812eeafc154ab46f32ebf5593
Andy Chiu <achiu8@gmail.com>
2014-10-16 19:56:24 -0700

    stuff

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,97 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  function updateMainPanel(response) {
+    $('#main-panel').html(response);
+  }
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('#login').on('submit', function(e) {
+    e.preventDefault();
+
+    // returns hash of form input data
+    var formData = $(this).serialize();
+
+    authenticateUser(formData);
+  });
+
+  function authenticateUser(formData) {
+    var request = $.ajax({
+      url: '/login',
+      type: 'POST',
+      data: formData
+    });
+
+    request.done(displayMainPage);
+  }
+
+  function displayMainPage(response) {
+    $('#container').html(response);
+  }
+
+  $('#container').on('click', 'a.view-survey', function(e) {
+    e.preventDefault();
+
+    var id = $(this).attr('id');
+
+    viewSurvey(id);
+  });
+
+  function viewSurvey(id) {
+    var request = $.ajax({
+      url: '/survey',
+      type: 'POST',
+      data: {id: id}
+    });
+
+    request.done(updateMainPanel);
+  }
+
+  $('#container').on('click', 'button#create-new-survey', function(e) {
+    e.preventDefault();
+
+    createSurvey();
+  });
+
+  function createSurvey() {
+    var request = $.ajax({
+      url: '/survey/new',
+      type: 'GET',
+    });
+
+    request.done(updateMainPanel);
+  }
+
+  $('#container').on('submit', '#new-survey-form', function(e) {
+    e.preventDefault();
+
+    var formData = $(this).serialize();
+
+    addQuestion(formData);
+  });
+
+  function addQuestion(formData) {
+    var request = $.ajax({
+      url: '/question/new',
+      type: 'POST',
+      data: formData
+    });
+
+    request.done(updateMainPanel);
+  }
+
+  $('#container').on('submit', '#new-question-form', function(e){
+    e.preventDefault();
+
+    var formData = $(this).serialize();
+
+    addChoice(formData);
+  });
+
+  function addChoice(formData){
+    var request = $.ajax({
+      url: '/choice/new',
+      type: 'POST',
+      data: formData
+    });
+
+    request.done(updateMainPanel);
+  }
 });
